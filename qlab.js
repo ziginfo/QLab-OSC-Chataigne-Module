967cc9e513dc34a203692b58a626579c83c08694
ziginfo <34913387+ziginfo@users.noreply.github.com>
2024-06-23 13:31:40 +0200

    Add files via upload

--- qlab.js
+++ qlab.js
@@ -3,8 +3,9 @@
 var myParameters = {};
 var myValues = {};
 var workspace_name = local.parameters.workspaceID.get() ;
-var cuecount = local.parameters.numberOfCues.get()  ;
+var cuecount = local.parameters.countOfCues.get()  ;
 var workspaceNo = local.values.workspaceID.get() ;
+var now = util.getTime();
 var permFb  ;
 var names = [];
 var requests = [];
@@ -14,7 +15,8 @@ var wild = "*" ;
 var parse ;
 var cueNo ;
 var cueNamSho = 1 ;
-var fbrate = 2 ;
+var fbRate = 2 ;
+var selCue ;
 
 var cueColors = ["none","berry","blue","crimson","cyan","forest","gray","green","hot pink","indigo","lavender","magenta","midnight","olive","orange","peach","plum","purple","red","sky blue","yellow"] ;
 
@@ -56,8 +58,8 @@ function update(deltaTime) {
 		
 		var now = util.getTime();
 		if(now > TSSendAlive) {
-		var fbrate = local.parameters.setFeedbackRate.get() ;
-			TSSendAlive = now + fbrate ; 
+		var fbRate = local.parameters.setFeedbackRate.get() ;
+			TSSendAlive = now + fbRate ; 
 			keepAlive(); 
 		 } }
 		
@@ -67,6 +69,7 @@ function keepAlive() {
 		local.send ("/runningCues/shallow") ;
 		local.send ("/selectedCues/shallow") ;
 		local.send ("/cue/"+cueNo+"/currentFileTime") ;
+		
 //		local.send("/cue/selected/name") ;
 //		local.send ("/alwaysReply" ,1) ;
 //		local.send ("/updates" ,1) ;
@@ -79,8 +82,15 @@ function keepAlive() {
 
 function moduleValueChanged(value) { 
 	if (value.name == "syncCueNames"){
-	
 	local.send ("/cueLists") ;	
+	}
+	
+	if (value.name == "cueNumber"){
+	selCue = local.values.cueInfos.cueNumber.get() ;
+	local.send ("/cue/"+selCue+"/notes") ;	
+	local.send ("/cue/"+selCue+"/listName") ;
+	local.send ("/cue/"+selCue+"/colorName") ;
+	local.send ("/cue/"+selCue+"/duration") ;
 	}
 	
 	if (value.name == "resetCueNames"){
@@ -94,6 +104,7 @@ function moduleValueChanged(value) {
 	local.send ("/runningCues/shallow") ;
 	local.send ("/selectedCues/shallow") ;
 	local.send ("/cue/"+cueNo+"/currentFileTime") ;
+	local.send ("/workspaces") ;
 //	local.send ("/alwaysReply" ,1) ;
 //	local.send ("/cueLists") ;
 //	local.send("/cue/selected/name") ;
@@ -109,14 +120,14 @@ function moduleValueChanged(value) {
 	
 // delete older Cue-Names	
 	if (value.name == "activeCueList"){
-	var count = local.values.cueNames.numberOfCues.get();
+	var count = local.values.cueNames.countOfCues.get();
 	for (n=1 ; n<=count ; n++)
  	{var child = "cue"+n ;
 	local.values.cueNames.getChild(child).set("");}  }
 	
 //  create Cue-Name-Lines
-	if (value.name == "activeCueList" || value.name == "syncCueNames" || value.name == "numberOfCues"){
-	cuecount = local.values.cueNames.numberOfCues.get()  ;
+	if (value.name == "activeCueList" || value.name == "syncCueNames" || value.name == "countOfCues"){
+	cuecount = local.values.cueNames.countOfCues.get()  ;
 //	local.values.cueNames.test.set(value.val) ;
 	for (var n = 1; n <= cuecount; n++) {
 			local.values.cueNames.addStringParameter("Cue "+n, "", "");  } }
@@ -131,6 +142,7 @@ function moduleValueChanged(value) {
 
 function moduleParameterChanged(param) { 
 	if (param.name == "sync"){
+	local.send ("/workspaces") ;
 	local.send ("/cueLists") ;
 	}
 }	
@@ -151,6 +163,45 @@ function oscEvent(address, args) {
 	}
 */	
 
+// >>>>>>>>> Get and Set Workspace
+	if (address == "/reply/workspaces" ) {
+	var parse = JSON.parse(args[0]) ;
+	var wsname = parse.data[0].displayName ;
+	local.values.workspaceName.set(wsname);
+	local.parameters.workspaceName.set(wsname);
+	}
+	
+// >>>>>>>>> Selected Cue Infos
+	if (local.match(address,"/reply/cue/*/notes")) {
+	var parse = JSON.parse(args[0]) ;
+	var notes = parse.data ;
+	if (notes == "") { notes = "No Notes...!" ;}
+	local.values.cueInfos.notes.set(notes);
+	}
+	if (local.match(address,"/reply/cue/*/listName")) {
+	var parse = JSON.parse(args[0]) ;
+	var name = parse.data ;
+	local.values.cueInfos.cueName.set(name);
+	}
+	if (local.match(address,"/reply/cue/*/duration")) {
+	var parse = JSON.parse(args[0]) ;
+	var dur = parse.data ;
+	local.values.cueInfos.cueDuration.set(dur);
+	}
+	if (local.match(address,"/reply/cue/*/colorName")) {
+	var parse = JSON.parse(args[0]) ;
+	var col = parse.data ;
+	if (col == "none"){ var r = 0; var g = 0; var b = 0 ;}
+	else if (col == "red"){ var r = 1; var g = 0; var b = 0 ;}
+	else if (col == "orange"){ var r = 1; var g = 0.5; var b = 0 ;}
+	else if (col == "green"){ var r = 0; var g = 1; var b = 0 ;}
+	else if (col == "blue"){ var r = 0; var g = 0; var b = 1 ;}
+	else if (col == "purple"){ var r = 1; var g = 0; var b = 1 ;}
+	else if (col == "yellow"){ var r = 1; var g = 1; var b = 0 ;}
+	else if (col == "skyblue"){ var r = 0.3; var g = 1; var b = 1 ;}
+	local.values.cueInfos.cueColor.set(r,g,b);
+	}
+	
 // >>>>>>>>> Get and Set selected and playing Cue
 	if (local.match(address,"/reply/workspace/*/runningCues/shallow")) {
 	var parse = JSON.parse(args[0]) ;
@@ -183,13 +234,13 @@ function oscEvent(address, args) {
 // get CueLists-Number
 	var lnumb = parse.data.length ;
 	local.values.cueNames.countOfCueLists.set(lnumb);
-	local.parameters.numberOfCueLists.set(lnumb);
+	local.parameters.countOfCueLists.set(lnumb);
 //	local.values.cueListCount.set(lnumb);
 // get Cues-Number
 	var numb = parse.data[cln].cues.length ;
-	local.values.cueNames.numberOfCues.set(numb);
+	local.values.cueNames.countOfCues.set(numb);
 //	local.values.cuesCount.set(numb);
-	local.parameters.numberOfCues.set(numb);
+	local.parameters.countOfCues.set(numb);
 //  insert CueList Name
 	var listname = parse.data[cln].listName ;
 	local.values.cueNames.cueListName.set(listname);
@@ -236,7 +287,11 @@ function show_playing() {
 //		 REGULAR FUCNTIONS
 //=========================================================
 
-// >>>>>>> CUE CONTROL
+// >>>>>>> MAIN CONTROL
+
+function main_control(val) {
+	local.send(val) ;	
+}
 
 function go() {
 	local.send("/go") ;	
@@ -248,10 +303,6 @@ function pause() {
 
 function resume() {
 	local.send("/resume") ;	
-}
-
-function stop_cue(cue) {
-	local.send("/cue/"+cue+"/stop") ;	
 }
 
 function back() {
@@ -274,30 +325,21 @@ function stop_intime(time) {
 	local.send("/panicInTime", time) ;	
 }
 
-function set_next(cue) {
-	local.send("/cue/"+cue+"/loadAndSetPlayhead");	
-}
-
-function start_next(cue) {
-	local.send("/cue/"+cue+"/startAndAutoloadNext");	
-}
 
 function reset(no) {
+	local.send("/reset") ;	
+}
+
+function cue_reset(no) {
 	local.send("/cue/"+no+"/reset") ;	
 }
 
-function reset_act(no) {
-	local.send("/cue/cueNumber/reset") ;	
-}
 
-
-function prewait_act(val) {
-	
+function prewait_act(val) {	
 	local.send("/cue/selected/preWait" , val) ;	
 }
 
-function postwait_act(val) {
-	
+function postwait_act(val) {	
 	local.send("/cue/selected/postWait" , val) ;	
 }
 
@@ -315,18 +357,104 @@ function playhead_cue() {
 	local.send("/cue/playhead/name") ;	
 }
 
-// >>>>>>>>>>> CUE ACTIONS
+// >>>>>>>>>>> CUE PLAY
 
-function rename_cue(cue, name) {
-	local.send("/cue/"+cue+"/name", name);	
+function cue_set(no, val) {
+	local.send("/cue/"+no+val) ;	
 }
 
-function renumber_cue(cue, numb) {
-	local.send("/cue/"+cue+"/number", numb);	
+function cue_go(no) {
+	local.send("/cue/"+no+"/go") ;	
+}
+
+function cue_stop(no) {
+	local.send("/cue/"+no+"/stop") ;	
+}
+
+function cue_pause(no) {
+	local.send("/cue/"+no+"/pause") ;	
+}
+
+function cue_resume(no) {
+	local.send("/cue/"+no+"/resume") ;	
+}
+
+function cue_pause(no) {
+	local.send("/cue/"+no+"/togglePause") ;	
+}
+
+function start_next(no) {
+	local.send("/cue/"+no+"/startAndAutoloadNext");	
+}
+
+function setload(no) {
+	local.send("/cue/"+no+"/loadAndSetPlayhead");	
+}
+
+function set_next(no) {
+	local.send("/playhead/"+no);	
+}
+
+function cue_load(no) {
+	local.send("/cue/"+no+"/load") ;	
+}
+
+function cue_prewait(no,val) {	
+	local.send("/cue/"+no+"/preWait" , val) ;	
+}
+
+function cue_postwait(no,val) {	
+	local.send("/cue/"+no+"/postWait" , val) ;	
+}
+
+function cue_timeload(no,val) {	
+	local.send("/cue/"+no+"/loadAt" , val) ;	
+}
+
+function cue_preview(no) {	
+	local.send("/cue/"+no+"/preview") ;	
+}
+
+// >>>>>>>>>>> CUE ACTIONS
+
+function rename_cue(no, val) {
+	local.send("/cue/"+no+"/name", val);	
+}
+
+function cue_notes(no, val) {
+	local.send("/cue/"+no+"/notes", val);	
+}
+
+function cue_color(no, col) {
+	local.send("/cue/"+no+"/colorName", col);	
+}
+
+function cue_continue(no, val) {
+	local.send("/cue/"+no+"/continueMode", val);	
+}
+
+function cue_target(no, val) {
+	local.send("/cue/"+no+"/cueTargetNumber", val);	
+}
+
+function renumber_cue(no, val) {
+	local.send("/cue/"+no+"/number", val);	
 }
 
 function renumber_allcues(val1, val2) {
 	local.send("/renumber", [val1, val2]) ;
+}
+
+function cue_duration(no, val) {
+	local.send("/cue/"+no+"/duration", val);	
+}
+
+function cue_flagged(no, val) {
+	local.send("/cue/"+no+"/flagged", val);	
+}
+
+function cue_autoload(no, val) {
+	local.send("/cue/"+no+"/autoLoad", val);	
 }
 
 // >>>>>>>>>>> AUDIO VOLUME
@@ -348,7 +476,6 @@ function actVol_full() {
 	local.send("/cue/active/level/0/0", 12) ;	
 }
 
-
 //>>>>>>>>>>>> OSC Requests
 
 function request_names() {
@@ -367,6 +494,14 @@ function request_uniqueids() {
 	local.send("/cueLists/uniqueIDs") ;
 }
 
+function version() {
+	local.send("/version") ;
+}
+
+function workspaces() {
+	local.send("/workspaces") ;
+}
+
 function reply(val) {
 	local.send("/alwaysReply" , val);	
 }
@@ -374,4 +509,74 @@ function reply(val) {
 function updates(val) {
 	local.send("/updates", val);	
 }
+
+//>>>>>>>>>>>> Actions
+
+function midiin_toggle() {
+	local.send("/overrides/toggleMidiInput") ;
+}
+
+function midiout_toggle() {
+	local.send("/overrides/toggleMidiOutput") ;
+}
+
+function midiin_enable(val) {
+	local.send("/overrides/midiInputEnabled") ;
+}
+
+function midiout_enable(val) {
+	local.send("/overrides/midiOutputEnabled") ;
+}
+
+function dmx_toggle() {
+	local.send("/overrides/toggleDmxOutput") ;
+}
+
+function dmx_enable(val) {
+	local.send("/overrides/dmxOutputEnabled") ;
+}
+
+function mscin_toggle() {
+	local.send("/overrides/toggleMscInput") ;
+}
+
+function mscout_toggle() {
+	local.send("/overrides/toggleMscOutput") ;
+}
+
+function mscin_enable(val) {
+	local.send("/overrides/mscInputEnabled") ;
+}
+
+function mscout_enable(val) {
+	local.send("/overrides/mscOutputEnabled") ;
+}
+
+function sysexin_toggle() {
+	local.send("/overrides/toggleSysexInput") ;
+}
+
+function sysexout_toggle() {
+	local.send("/overrides/toggleSysexOutput") ;
+}
+
+function sysexin_enable(val) {
+	local.send("/overrides/sysexInputEnabled") ;
+}
+
+function sysexout_enable(val) {
+	local.send("/overrides/sysexOutputEnabled") ;
+}
+
+function toggle_override() {
+	local.send("/toggleOverrideWindow") ;
+}
+
+function toggle_timecode() {
+	local.send("/toggleTimecodeWindow") ;
+}
+
+
+
+
 
